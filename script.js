ab65a3ec9d3dea15de975f5acb1e23581157bbd6
elysthetruechi <kaluelizabeth29@gmail.com>
2023-05-22 13:45:58 +0100

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body")
+sidebar = body.querySelector("nav");
+sidebarToggle = body.querySelector(".sidebar-toggle");
+
+sidebarToggle.addEventListener("click", () =>{
+ sidebar.classList.toggle("close");
+})
